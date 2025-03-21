9221ea2746abe8d19512e813964c24cd85c5983b
davidchavezc <davidchavezc05@gmail.com>
2025-03-21 00:56:13 -0600

    Add example.py file

---
+++ ejemplo.py
@@ -0,0 +1,6 @@
+arreglo = [4, 7, 9, 5, 20, 8, 32, 2, 1, 56]
+suma = 0
+for i in range(0, len(arreglo)):
+    suma+=arreglo[i]
+print(f"La suma del arreglo es {suma}")
+print(f"El promedio del arreglo es {suma/len(arreglo)}")
